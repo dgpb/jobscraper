0e5ddcc050dcdad667c309b835f4e04d3c1856c7
daniel <dgpb27@gmail.com>
2020-10-14 23:11:18 -0500

    ya scrapea!

--- jobscraper/spiders/jobspider.py
+++ jobscraper/spiders/jobspider.py
@@ -1,5 +1,5 @@
 import scrapy
-from scrapy.spiders import CrawlSpider
+from scrapy.spiders import CrawlSpider, Rule
 from ..items import JobscraperItem
 from scrapy.linkextractors import LinkExtractor
 
@@ -9,18 +9,19 @@ class JobscraperSpider(CrawlSpider):
     name ='jobspider'
     start_urls = ['https://mott.pe/bolsa/ofertas?oferta=&lugar=&categoria=']
 
-    def parse(self, response):
-        link = response.xpath('//div[@class="list"]/div/a')
-        for link in self.link_extract_links(response):
-            yield Request(link.url, callback=self.parse)
+    rules = (
 
-    def parse(self, response):
+        Rule(LinkExtractor(allow=('/bolsa/166',)), follow=True, callback='parse_item'),
+
+    )
+
+    def parse_item(self, response):
         items = JobscraperItem()
 
         job_title = response.xpath('//h1/text()').extract()
         company = response.xpath('//h2/b/text()').extract()
         company_url = response.xpath('//div[@class="pull-left"]/a/text()').extract()
-        description = response.xpath('//div[@class="aviso"]/text()').extract()
+        description = response.xpath('string(//div[@class="aviso"])').extract()
         salary = response.xpath('//div[@id="aviso"]/p[1]/text()').extract()
         city = response.xpath('//div[@id="aviso"]/p[2]/text()').extract()
         district = response.xpath('//div[@id="aviso"]/p[5]/text()').extract()
